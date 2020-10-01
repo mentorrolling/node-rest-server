1086b4504c9af08c3936aa68ecbf00dec2ea4719
pmarino2013@gmail.com <pmarino2013@gmail.com>
2020-10-01 16:53:42 -0300

    Borrando direccion de la url de conexion con heroku

--- server/config/config.js
+++ server/config/config.js
@@ -15,8 +15,6 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
   urlDB = "mongodb://localhost:27017/cafe";
 } else {
-  // urlDB =
-  //   "mongodb+srv://mentor:5FhxzjSRbk1BCtti@cluster0.ddox7.mongodb.net/cafe";
   urlDB = process.env.MONGO_URI;
 }
 
